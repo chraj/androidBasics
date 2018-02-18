675f48a391abe844eb762188a36104b0df65c91b
DP singh <dharmendrascpm@gmail.com>
2018-02-18 23:07:50 +0530

    Added example of broadcast

--- app/src/main/java/com/bot/androidbasic/AsynTaskActivity.java
+++ app/src/main/java/com/bot/androidbasic/AsynTaskActivity.java
@@ -1,6 +1,11 @@
 package com.bot.androidbasic;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +15,9 @@ public class AsynTaskActivity extends AppCompatActivity {
 
 
     TextView textView;
+    TextView textViewconnectivity;
 
-    static AsyncTask<Void, Integer, Void> execute;
+    static MtAsyncTask execute;
 
     @SuppressLint("StaticFieldLeak")
     @Override
@@ -20,54 +26,77 @@ public class AsynTaskActivity extends AppCompatActivity {
         setContentView(R.layout.activity_asyn_task);
 
         textView = findViewById(R.id.progressCount);
+        textViewconnectivity = findViewById(R.id.connectivity);
         //way of doing something on backgroud thread
         //this is basically wrapper that is given as android API to use thread
 
-
-        execute = new AsyncTask<Void, Integer, Void>() {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                textView.setText("Thread Started");
-            }
-
-
-            //worker thread
-            @Override
-            protected Void doInBackground(Void... voids) {
-                int n = 0;
-                while (n < 10) {
-                    n++;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    onProgressUpdate(n);
-                }
-
-                return null;
-            }
-
-
-            @SuppressLint("DefaultLocale")
-            @Override
-            protected void onProgressUpdate(Integer... values) {
-                super.onProgressUpdate(values);
-                textView.setText(String.format("Counting =%d", values.length > 0 ? values[0] : 0));
-            }
-
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                textView.setText("Thread Ended");
-            }
-
-
-        }.execute();
+        execute = new MtAsyncTask();
     }
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(receiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+
+    BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction() != null && intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+                textViewconnectivity.setText(intent.getExtras().toString());
+            }
+
+        }
+    };
+
+    private class MtAsyncTask extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            textView.setText("Thread Started");
+        }
+
+
+        //worker thread
+        @Override
+        protected Void doInBackground(Void... voids) {
+            int n = 0;
+            while (n < 10) {
+                n++;
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                onProgressUpdate(n);
+            }
+
+            return null;
+        }
+
+
+        @SuppressLint("DefaultLocale")
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            textView.setText(String.format("Counting =%d", values.length > 0 ? values[0] : 0));
+        }
+
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            textView.setText("Thread Ended");
+        }
+
+    }
 
 }
